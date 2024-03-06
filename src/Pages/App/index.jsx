29d7ce295e3212773bfcc09f77bce4e82ef06880
Skabas18 <sebascabalq@gmail.com>
2024-03-05 22:50:57 -0500

    Creacion de carpetas para las paginas

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,26 @@
+import Home from '../Home/index'
+import MyAccount from '../MyAccount/index'
+import MyOrder from '../MyOrder/index'
+import MyOrders from '../MyOrders/index'
+import NotFound from '../NotFound/index'
+import SignIn from '../SignIn/index'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-100'>
+        <Home />
+        <MyAccount />
+        <MyOrder />
+        <MyOrders />
+        <NotFound />
+        <SignIn />
+      </div>
+    </>
+  )
+}
+
+export default App
