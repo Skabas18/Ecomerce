a793acc5da2d89322ae06da4b2ae84736dab5a56
Skabas18 <sebascabalq@gmail.com>
2024-03-09 12:27:22 -0500

    Creacion de las cards en grids@

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../../Components/Layaout'
-import Card from '../../Components/Card/index.jsX'
+import Card from '../../Components/Card/index.jsx'
+import axios from "axios";
 function Home() {
+  const [items, setItems] = useState(null);
+  const getUrl = 'https://fakestoreapi.com/products';
+  useEffect(() => {
+    axios.get(getUrl).then((response) => {
+      console.log(response)
+      setItems(response.data)
+    })
+  }, [])
   return (
     <Layout>
-      Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {items?.map((item) => {
+            return <Card key={item.id} data={item} />
+          })
+        }
+      </div>
     </Layout>
   )
 }
