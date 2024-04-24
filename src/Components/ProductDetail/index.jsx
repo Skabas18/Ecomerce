b4dd3c4ad3003ed5ccaaa607574922b3e26ff177
Sebastian Cabal <sebascabalq@gmail.com>
2024-04-24 07:59:22 -0500

    Creacion de logica para abrir el detalle del producto

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import { useContext } from 'react'
 import './ProductDetail.css'
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from '../../Context'
 function ProductDetail() {
+  const context = useContext(ShoppingCartContext)
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
-        <div className='flex justify-between items-center p-6'>
-            <h2 className='font-medium text-xl'>Detail</h2>
-                <div>
-                    <XMarkIcon className='h-6 w-6 text-black'/>
-                </div>
+    <aside className={`${context.isPorductDetailOpen ? 'flex' :'hidden' } product-detail  flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+      <div className='flex justify-between items-center p-6'>
+        <h2 className='font-medium text-xl'>Detail</h2>
+        <div>
+          <XMarkIcon className='h-6 w-6 text-black' />
         </div>
+      </div>
     </aside>
   )
 }
 
-export {ProductDetail}
+export { ProductDetail }
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,10 +4,22 @@ const ShoppingCartContext = createContext()
 
 const ShoppingCartProvider = ({ children }) => {
     const [count, setCount] = useState(0);
+    const [isPorductDetailOpen, setIsPorductDetailOpen] = useState(false);
+
+    const openProductDetail = () =>{
+        setIsPorductDetailOpen(true)
+    }
+    const closeProductDetail = () =>{
+        setIsPorductDetailOpen(false)
+    }
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isPorductDetailOpen
         }}>
             {children}
         </ShoppingCartContext.Provider>
