3671794d8113073900d04d5a2793259d04a1a627
Sebastian Cabal <sebascabalq@gmail.com>
2024-04-24 08:37:41 -0500

    Agregar la inforación del producto en el detalle del producto

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,15 +3,16 @@ import { createContext, useState } from 'react'
 const ShoppingCartContext = createContext()
 
 const ShoppingCartProvider = ({ children }) => {
+    //Shopping Cart . Increment quantity
     const [count, setCount] = useState(0);
-    const [isPorductDetailOpen, setIsPorductDetailOpen] = useState(false);
 
-    const openProductDetail = () =>{
-        setIsPorductDetailOpen(true)
-    }
-    const closeProductDetail = () =>{
-        setIsPorductDetailOpen(false)
-    }
+    //Poduct Detail . Open/Close
+    const [isPorductDetailOpen, setIsPorductDetailOpen] = useState(false);
+    const openProductDetail = () =>{ setIsPorductDetailOpen(true) }
+    const closeProductDetail = () =>{ setIsPorductDetailOpen(false) }
+    
+    //Poduct Detail . Show product
+    const [productToShow, setProductToShow] = useState({})
 
     return (
         <ShoppingCartContext.Provider value={{
@@ -19,7 +20,9 @@ const ShoppingCartProvider = ({ children }) => {
             setCount,
             openProductDetail,
             closeProductDetail,
-            isPorductDetailOpen
+            isPorductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
