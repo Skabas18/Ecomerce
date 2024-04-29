f8cdde9bef9ddd178a3326a28547a8bb6069d81b
Sebastian Cabal <sebascabalq@gmail.com>
2024-04-29 07:33:37 -0500

    Creacion de logica para eliminar productos del carrito de compras

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -6,6 +6,11 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 function CheckoutSideMenu() {
     const context = useContext(ShoppingCartContext)
 
+    const handleDelete = (id) =>{
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
+
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu  flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
@@ -17,7 +22,7 @@ function CheckoutSideMenu() {
             <div className='px-6 overflow-y-scroll'>
                 {
                     context.cartProducts.map(product => (
-                        <OrderCard key={product.id} title={product.title} imageUrl={product.image} price={product.price} />
+                        <OrderCard key={product.id} id={product.id} title={product.title} imageUrl={product.image} price={product.price} handleDelete={handleDelete}/>
                     )
                     )
                 }
