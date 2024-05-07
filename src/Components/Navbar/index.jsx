c3364a3d422749f18193a7afdf90c193cf51e657
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-07 09:33:10 -0500

    Agregar rutas protegidas para el usuario cuando no ha inciado sesion, se crea tambien el componente de Shopping Cart

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,146 +1,153 @@
 import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { ShoppingCart } from '../ShoppingCart'
 import { ShoppingCartContext } from '../../Context'
-import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
-    const context = useContext(ShoppingCartContext)
-    const activeStyle = 'underline underline-offset-4'
-  
-    // Sign Out
-    const signOut = localStorage.getItem('sign-out')
-    const parsedSignOut = JSON.parse(signOut)
-    const isUserSignOut = context.signOut || parsedSignOut
-  
-    const handleSignOut = () => {
-      const stringifiedSignOut = JSON.stringify(true)
-      localStorage.setItem('sign-out', stringifiedSignOut)
-      context.setSignOut(true)
-    }
-  
-    const renderView = () => {
-      if (isUserSignOut) {
-        return (
+  const context = useContext(ShoppingCartContext)
+  const activeStyle = 'underline underline-offset-4'
+
+  // Sign Out
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+  const isUserSignOut = context.signOut || parsedSignOut
+
+  // Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
+
+  const handleSignOut = () => {
+    const stringifiedSignOut = JSON.stringify(true)
+    localStorage.setItem('sign-out', stringifiedSignOut)
+    context.setSignOut(true)
+  }
+
+  const renderView = () => {
+    if (hasUserAnAccount && !isUserSignOut) {
+      return (
+        <li>
+          <NavLink
+            to="/sign-in"
+            className={({ isActive }) => isActive ? activeStyle : undefined}
+            onClick={() => handleSignOut()}>
+            Sign out
+          </NavLink>
+        </li>
+      )
+    } else {
+      return (
+        <>
+          <li className='text-black/60'>
+            teff@platzi.com
+          </li>
           <li>
             <NavLink
-              to="/sign-in"
-              className={({ isActive }) => isActive ? activeStyle : undefined }
+              to='/my-orders'
+              className={({ isActive }) => isActive ? activeStyle : undefined}>
+              My Orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/my-account'
+              className={({ isActive }) => isActive ? activeStyle : undefined}>
+              My Account
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/sign-in'
+              className={({ isActive }) => isActive ? activeStyle : undefined}
               onClick={() => handleSignOut()}>
               Sign out
             </NavLink>
           </li>
-        )
-      } else {
-        return (
-          <>
-            <li className='text-black/60'>
-              teff@platzi.com
-            </li>
-            <li>
-              <NavLink
-                to='/my-orders'
-                className={({ isActive }) => isActive ? activeStyle : undefined}>
-                My Orders
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/my-account'
-                className={({ isActive }) => isActive ? activeStyle : undefined}>
-                My Account
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/sign-in'
-                className={({ isActive }) => isActive ? activeStyle : undefined}
-                onClick={() => handleSignOut()}>
-                Sign out
-              </NavLink>
-            </li>
-          </>
-        )
-      }
+        </>
+      )
     }
-  
-    return (
-      <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
-        <ul className='flex items-center gap-3'>
-          <li className='font-semibold text-lg'>
-            <NavLink to='/'>
-              Shopi
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/'
-              onClick={() => context.setSearchByCategory()}
-              className={({ isActive }) =>
-                isActive ? activeStyle : undefined
-              }>
-              All
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/clothes'
-              onClick={() => context.setSearchByCategory('clothes')}
-              className={({ isActive }) =>
-                isActive ? activeStyle : undefined
-              }>
-              Clothes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/electronics'
-              onClick={() => context.setSearchByCategory('electronics')}
-              className={({ isActive }) =>
-                isActive ? activeStyle : undefined
-              }>
-              Electronics
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/furnitures'
-              onClick={() => context.setSearchByCategory('furnitures')}
-              className={({ isActive }) =>
-                isActive ? activeStyle : undefined
-              }>
-              Furnitures
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/toys'
-              onClick={() => context.setSearchByCategory('toys')}
-              className={({ isActive }) =>
-                isActive ? activeStyle : undefined
-              }>
-              Toys
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/others'
-              onClick={() => context.setSearchByCategory('others')}
-              className={({ isActive }) =>
-                isActive ? activeStyle : undefined
-              }>
-              Others
-            </NavLink>
-          </li>
-        </ul>
-        <ul className='flex items-center gap-3'>
-          {renderView()}
-          <li className='flex items-center'>
-            <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon>
-            <div>{context.cartProducts.length}</div>
-          </li>
-        </ul>
-      </nav>
-    )
   }
+
+  return (
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink to={`${isUserSignOut ? '/sign-in' : '/'}`}>
+            Shopi
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/'
+            onClick={() => context.setSearchByCategory()}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/clothes'
+            onClick={() => context.setSearchByCategory('clothes')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Clothes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Electronics
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/furnitures'
+            onClick={() => context.setSearchByCategory('furnitures')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Furnitures
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/toys'
+            onClick={() => context.setSearchByCategory('toys')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Toys
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/others'
+            onClick={() => context.setSearchByCategory('others')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Others
+          </NavLink>
+        </li>
+      </ul>
+      <ul className='flex items-center gap-3'>
+        {renderView()}
+        <li className='flex items-center'>
+          <ShoppingCart />
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
 export { Navbar }
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -6,19 +6,36 @@ import { MyOrders } from '../MyOrders'
 import { NotFound } from '../NotFound'
 import { MyAccount } from '../MyAccount'
 import { Navbar } from '../../Components/Navbar'
-import { ShoppingCartProvider } from '../../Context'
-import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartContext, ShoppingCartProvider, initializeLocalStorage } from '../../Context'
+import { useRoutes, BrowserRouter, Navigate } from 'react-router-dom'
 import { CheckoutSideMenu } from '../../Components/CheckoutSideMenu'
+import { useContext } from 'react'
 
 const AppRoutes = () => {
-  let routes = useRoutes([
-    { path: '/', element: <Home /> },
-    { path: '/clothes', element: <Home /> },
-    { path: '/electronics', element: <Home /> },
-    { path: '/furnitures', element: <Home /> },
-    { path: '/toys', element: <Home /> },
-    { path: '/others', element: <Home /> },
+  const context = useContext(ShoppingCartContext)
+  
+  // Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
 
+  //Sign Out
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
+  const isUserSignOut = context.signOut || parsedSignOut
+
+
+  let routes = useRoutes([
+    { path: '/', element: hasUserAnAccount && !isUserSignOut? <Home/> : <Navigate replace to={'/sign-in'}/> },
+    { path: '/clothes', element: hasUserAnAccount && !isUserSignOut? <Home/> : <Navigate replace to={'/sign-in'}/> },
+    { path: '/electronics', element: hasUserAnAccount && !isUserSignOut? <Home/> : <Navigate replace to={'/sign-in'}/> },
+    { path: '/furnitures', element: hasUserAnAccount && !isUserSignOut? <Home/> : <Navigate replace to={'/sign-in'}/> },
+    { path: '/toys', element: hasUserAnAccount && !isUserSignOut? <Home/> : <Navigate replace to={'/sign-in'}/> },
+    { path: '/others', element: hasUserAnAccount && !isUserSignOut? <Home/> : <Navigate replace to={'/sign-in'}/> },
 
     { path: '/my-account', element: <MyAccount /> },
     { path: '/my-order', element: <MyOrder /> },
@@ -31,6 +48,7 @@ const AppRoutes = () => {
   return routes
 }
 const App = () => {
+  initializeLocalStorage()
   return (
     <ShoppingCartProvider>
       <BrowserRouter>
